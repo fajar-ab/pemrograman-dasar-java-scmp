b20f216653b7db200939720f4e12bf568852f943
Fajar-ab <fajarfanz378@gmail.com>
2020-11-18 20:48:48 +0700

    Program Demo Continue

---
+++ Pertemuan 4/Demo Continue/src/democontinue/DemoContinue.java
@@ -0,0 +1,22 @@
+package democontinue;
+
+public class DemoContinue {
+    
+    public static void main(String[] args) {
+    	int counterDiabaikan = 0;
+
+    	System.out.println("\nPROGRAM DEMO CONTINUE");
+    	System.out.println("----------------------");
+
+    	for(int counter = 1; counter <= 5; counter++) {
+    		if(counter == 3) {
+    			counterDiabaikan = counter;
+    			continue;
+    		} else {
+    			System.out.println("Mengecek counter ke-" + counter);
+    		}
+    	}
+
+    	System.out.println("\nPerulangan telah di hentikan oleh 'continue' pada counter ke-" + counterDiabaikan);
+    }
+}
