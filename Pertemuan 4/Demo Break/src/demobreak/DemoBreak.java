36271ce08e3c33169b6550cce24d7275ff9a284a
Fajar-ab <fajarfanz378@gmail.com>
2020-11-18 20:39:53 +0700

    Program Demo Break

---
+++ Pertemuan 4/Demo Break/src/demobreak/DemoBreak.java
@@ -0,0 +1,18 @@
+package demobreak;
+
+public class DemoBreak {
+    
+    public static void main(String[] args) {
+    	int counter = 0;
+
+    	System.out.println("\nPROGRAM DEMO BREAK");
+    	System.out.println("-------------------");
+
+    	while(counter < 100) {
+    		counter++;
+    		if(counter == 7) break;
+    	}
+
+    	System.out.println("Counter terakhir adalah: " + counter);
+    }
+}
