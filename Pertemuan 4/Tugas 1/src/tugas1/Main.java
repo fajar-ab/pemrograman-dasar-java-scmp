7ecb772f711af734078e08cf2efa96c1313ff212
Fajar-ab <fajarfanz378@gmail.com>
2020-11-18 22:44:31 +0700

    Memperbaiki proses prmograman

--- Pertemuan 4/Tugas 1/src/tugas1/Main.java
+++ Pertemuan 4/Tugas 1/src/tugas1/Main.java
@@ -7,19 +7,13 @@ package tugas1;
 public class Main {
     
     public static void main(String[] args) {
-		int bil = 101;
 
 		System.out.println("\nMENAMPILKAN BILANGAN GANJIL 100 - 186");
 		System.out.println("--------------------------------------");
 
-		for (int i = 1; i <= 100; i++) {
-			
-			System.out.println(bil);
-			bil += 2;
-
-			if(bil > 186) {
-				break;
-			}
+		for (int i = 101; i < 186; i++) {
+			System.out.println(i);
+			i += 1;
 		}
-    }
+	}
 }
