88f73ddb1164d8a4aaa5e4393163c9b7e9925744
Fajar-ab <fajarfanz378@gmail.com>
2020-11-28 01:48:45 +0700

    upload tugas pertemuan 4

---
+++ Pertemuan 4/Tugas 1/src/tugas1/Main.java
@@ -0,0 +1,19 @@
+/*
+	Tugas 1 - program untuk menampilkan bilangan ganjil dari 100 - 186
+*/
+
+package tugas1;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+		System.out.println("\nMENAMPILKAN BILANGAN GANJIL 100 - 186");
+		System.out.println("--------------------------------------");
+
+		for (int i = 101; i < 186; i++) {
+			System.out.println(i);
+			i += 1;
+		}
+	}
+}
