732dea0a534713f71c823f5ea9894d8a8896824e
Fajar-ab <fajarfanz378@gmail.com>
2020-12-07 20:32:36 +0700

    Modifikasi program

--- Pertemuan 5/Perkaian Matriks/src/com/perkalian/matriks/PerkalianMatriks.java
+++ Pertemuan 5/Perkaian Matriks/src/com/perkalian/matriks/PerkalianMatriks.java
@@ -72,9 +72,9 @@ public class PerkalianMatriks {
     public static void cetakHasil(int[][] matriks1, int[][] matriks2, int[][] matriks3, char op) {
     	for (int i = 0; i < matriks1.length; i++) {
     		/*-----------------------------------------------------*/
-    		for (int j = 0; j < matriks1.length; j++) {
-    			System.out.print(" " + matriks1[i][j]);
-    			if (i == matriks1.length/2) {
+    		for (int j = 0; j < matriks1[0].length; j++) {
+    			System.out.print("  " + matriks1[i][j]);
+    			if (i == (matriks1.length/2)) {
     				System.out.print(" "+ op +"");
     			}else {
     				System.out.print("  ");
@@ -83,15 +83,15 @@ public class PerkalianMatriks {
     		/*-----------------------------------------------------*/
     		for (int j = 0; j < matriks2[0].length; j++) {
     			System.out.print("  "+ matriks2[i][j]);
-    			if (i == matriks1.length/2) {
+    			if (i == (matriks1.length/2)) {
     				System.out.print(" =");
     			}else {
-    				System.out.print(" ");
+    				System.out.print("  ");
     			}
     		}
     		/*-----------------------------------------------------*/
     		for (int j = 0; j < matriks3[0].length; j++) {
-    			System.out.print(" " + matriks3[i][j]);
+    			System.out.print("  " + matriks3[i][j]);
     		}
 
     		System.out.println();
