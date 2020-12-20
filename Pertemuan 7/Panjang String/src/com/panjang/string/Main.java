ab37db24025edb21a44ca0a35218871211992dd4
Fajar-ab <fajarfanz378@gmail.com>
2020-12-21 01:14:56 +0700

    Program Panjang String - Pertemuan 7

---
+++ Pertemuan 7/Panjang String/src/com/panjang/string/Main.java
@@ -0,0 +1,25 @@
+package com.panjang.string;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    	String A = "Pemograman Berbasis Objek";
+    	String B = "Rekayasa Perangkat Lunak";
+    	String C, D, E;
+
+    	C = "Sistem Informasi";
+    	D = "STMIK Citara MAndiri";
+    	E = "Padangsidimpuan";
+
+    	int nA = A.length();
+    	int nB = B.length();
+    	int nC = C.length();
+
+    	System.out.println(A+ " --> Panjang String : " + nA);
+    	System.out.println(B+ " --> Panjang String : " + nB);
+    	System.out.println(C+ " --> Panjang String : " + nC);
+    	System.out.println(D+ " --> Panjang String : " + D.length());
+    	System.out.println(E+ " --> Panjang String : " + E.length());
+
+    }
+}
