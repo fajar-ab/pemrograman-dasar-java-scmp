fd7c879d248b3a4f05d688fdc1d267f4d92693d8
Fajar-ab <fajarfanz378@gmail.com>
2021-02-07 00:22:34 +0700

    Memperbaiki program Perkalian Matriks

--- Pertemuan 5/Perkaian Matriks/src/com/perkalian/matriks/PerkalianMatriks.java
+++ Pertemuan 5/Perkaian Matriks/src/com/perkalian/matriks/PerkalianMatriks.java
@@ -53,6 +53,11 @@ public class PerkalianMatriks {
     	int[][] hasil = jumlahMatriks(matriks1, matriks2);
     	System.out.println("\n Penjumlahan Matriks");
     	cetakHasil(matriks1, matriks2, hasil, '+');
+
+        // mengkalikan dua matriks dan menyetaknya di monitor
+        hasil = kaliMatriks(matriks1, matriks2);
+        System.out.println("\n Penjumlahan Matriks");
+        cetakHasil(matriks1, matriks2, hasil, 'x');
     }
 
     // fungsi menjumlahkan dua matriks
@@ -68,33 +73,50 @@ public class PerkalianMatriks {
     	return hasil;
     }
 
+    // fungsi mengkalikan dua matriks
+    public static int[][] kaliMatriks(int[][] matriks1, int[][] matriks2) {
+        int[][] hasil = new int[matriks1.length][matriks2[0].length];
+
+        for (int i = 0; i < hasil.length; i++) {
+            for (int j = 0; j < hasil[0].length; j++) {
+                for (int k = 0; k < matriks1[0].length; k++){
+                    hasil[i][j] += matriks1[i][k] * matriks2[k][j];
+                }
+            }
+        }
+
+        return hasil;
+    }
+
     //  fungsi mancetak hasil ke monitor
     public static void cetakHasil(int[][] matriks1, int[][] matriks2, int[][] matriks3, char op) {
-    	for (int i = 0; i < matriks1.length; i++) {
-    		/*-----------------------------------------------------*/
-    		for (int j = 0; j < matriks1[0].length; j++) {
-    			System.out.print("  " + matriks1[i][j]);
-    			if (i == (matriks1.length/2)) {
-    				System.out.print(" "+ op +"");
-    			}else {
-    				System.out.print("  ");
-    			}
-    		}
-    		/*-----------------------------------------------------*/
-    		for (int j = 0; j < matriks2[0].length; j++) {
-    			System.out.print("  "+ matriks2[i][j]);
-    			if (i == (matriks1.length/2)) {
-    				System.out.print(" =");
-    			}else {
-    				System.out.print("  ");
-    			}
-    		}
-    		/*-----------------------------------------------------*/
-    		for (int j = 0; j < matriks3[0].length; j++) {
-    			System.out.print("  " + matriks3[i][j]);
-    		}
 
-    		System.out.println();
-    	}
+        for (int i = 0; i < matriks1[0].length; i++) {
+            for (int j = 0; j < matriks1[0].length; j++) {
+                System.out.print("  " + matriks1[i][j]);
+            }
+            if (i == matriks1.length / 2) {
+                System.out.print(" " + op + "");
+            }else {
+                System.out.print("  ");
+            }
+                
+
+            for (int j = 0; j < matriks2[0].length; j++) {
+                System.out.print("  " + matriks2[i][j]);
+            }
+            if (i == matriks1.length / 2) {
+                System.out.print(" =");
+            }else {
+                System.out.print("  ");
+            }
+
+
+            for (int j = 0; j < matriks3[0].length; j++) {
+                System.out.print("  " + matriks3[i][j]);
+            }
+
+            System.out.println();
+        }
     }
 }
